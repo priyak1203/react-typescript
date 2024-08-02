32e7ab35d7563aa9fc62a72763d7ff5445bf14a6
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-02 14:47:55 +0530

    type annotations

---
+++ 01-TypeScript-Tutorial/src/01-TypeAnnotations-TypeInference/01.TypeAnnotations.ts
@@ -0,0 +1,23 @@
+/* ########## TYPE ANNOTATIONS ########## */
+
+/* ========= String Type ========= */
+let name: string = 'shakeAndBake';
+name = 'Adhiraj';
+name = name.toUpperCase();
+console.log(name);
+
+// name = 25 // not allowed
+
+/* ========= Number Type ========= */
+let amount: number = 25;
+amount = 25 - 4;
+console.log(amount);
+
+// amount ='Kavya' // not allowed
+
+/* ========= Boolean Type ========= */
+let isEligible: boolean = true;
+isEligible = false;
+console.log(isEligible);
+
+// isEligible = 'not eligible' // not allowed
