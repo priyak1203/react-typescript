514d7ba2374945c0d9d20676c2bad01a23f4aacb
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-02 14:49:37 +0530

    type inference

---
+++ 01-TypeScript-Tutorial/src/01-TypeAnnotations-TypeInference/02.TypeInference.ts
@@ -0,0 +1,23 @@
+/* ########## TYPE INFERENCE ########## */
+
+/* ========= String Type ========= */
+let name = 'shakeAndBake';
+name = 'Adhiraj';
+name = name.toUpperCase();
+console.log(name);
+
+// name = 25 // not allowed
+
+/* ========= Number Type ========= */
+let amount = 25;
+amount = 25 - 4;
+console.log(amount);
+
+// amount ='Kavya' // not allowed
+
+/* ========= Boolean Type ========= */
+let isEligible = true;
+isEligible = false;
+console.log(isEligible);
+
+// isEligible = 'not eligible' // not allowed
