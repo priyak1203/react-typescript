c4adc40593a0676e516c1ff353609968fbd9b1b3
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-06 19:11:35 +0530

    function parameters

---
+++ 01-TypeScript-Tutorial/src/04-FunctionParams-FunctionReturns/01.FunctionParams.ts
@@ -0,0 +1,9 @@
+/* ########## FUNCTION PARAMETERS ########## */
+
+function greet(name: string) {
+  console.log(`Hello there, ${name.toUpperCase()}`);
+}
+
+greet('rohit');
+// greet(31); // This will result in a TypeScript error
+// greet('nisha', 'shreya'); // This will result in a TypeScript error
