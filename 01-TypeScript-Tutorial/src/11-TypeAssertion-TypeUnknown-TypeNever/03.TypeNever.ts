c32bff2f917b98464df0b0ab0da9686a6b60c46f
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-15 15:39:03 +0530

    type never

---
+++ 01-TypeScript-Tutorial/src/11-TypeAssertion-TypeUnknown-TypeNever/03.TypeNever.ts
@@ -0,0 +1,44 @@
+/* ########## TYPE NEVER ########## */
+
+// let someValue: never = 0; // This will result in TypeScript Error
+
+type Theme = 'light' | 'dark';
+
+function checkTheme(theme: Theme): void {
+  if (theme === 'light') {
+    console.log('light theme');
+    return;
+  }
+  if (theme === 'dark') {
+    console.log('dark theme');
+    return;
+  }
+  theme; // theme is of type never here
+}
+
+enum Color {
+  Red,
+  Blue,
+  Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    case Color.Green:
+      return 'Green';
+
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${color}`);
+  }
+}
+
+console.log(getColorName(Color.Red));
+console.log(getColorName(Color.Blue));
+console.log(getColorName(Color.Green));
