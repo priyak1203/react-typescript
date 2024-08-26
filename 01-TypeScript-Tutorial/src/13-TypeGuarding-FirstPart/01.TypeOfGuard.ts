1be3caff9b13f0808841342b75bdad58cc5a3a9e
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-26 14:39:39 +0530

    typeof guard

---
+++ 01-TypeScript-Tutorial/src/13-TypeGuarding-FirstPart/01.TypeOfGuard.ts
@@ -0,0 +1,23 @@
+/* ########## TYPE GUARDS PART 1 ########## */
+
+// "typeof" guard
+
+type ValueType = string | number | boolean;
+
+let value: ValueType;
+const random = Math.random();
+value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
+
+function checkValue(value: ValueType): void {
+  if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+    return;
+  }
+  if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+    return;
+  }
+  console.log(`boolean: ${value}`);
+}
+
+checkValue(value);
