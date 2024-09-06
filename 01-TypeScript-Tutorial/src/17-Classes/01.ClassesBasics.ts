af73a34825ff3140fc0998ff298d446a15f25f09
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-06 14:18:44 +0530

    classes basics

---
+++ 01-TypeScript-Tutorial/src/17-Classes/01.ClassesBasics.ts
@@ -0,0 +1,14 @@
+/* ########## CLASSES BASICS ########## */
+
+class Book {
+  title: string;
+  author: string;
+
+  constructor(title: string, author: string) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+const deepWork = new Book('Deep Work', 'Cal Newport');
+console.log(deepWork);
