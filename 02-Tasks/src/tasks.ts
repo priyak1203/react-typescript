b82d52469de15d5cb65a120e468bcf28c3994c10
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-09 12:50:27 +0530

    non null assertion operator and optional chainging

--- 02-Tasks/src/tasks.ts
+++ 02-Tasks/src/tasks.ts
@@ -1,1 +1,22 @@
 console.log('TypeScript Loaded');
+
+// Case 1
+const btn = document.querySelector('.test-btn');
+
+// Element could be null
+btn?.addEventListener('click', () => {
+  console.log('Run time check with optional chaining');
+});
+
+if (btn) {
+  // Do something
+}
+
+// Case 2
+// Non null assertion operator
+
+const testBtn = document.querySelector('.test-btn')!;
+
+testBtn.addEventListener('click', () => {
+  console.log('Non Null Assertion Operator');
+});
