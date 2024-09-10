ce5df3a131cd8db187fbdf979d5805c78cb0b911
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-10 12:01:07 +0530

    setting up the elements and type for tasks

--- 02-Tasks/src/tasks.ts
+++ 02-Tasks/src/tasks.ts
@@ -1,30 +1,13 @@
 console.log('TypeScript Loaded');
 
-// Generic Element
-// has no access to button specific properties
-const btnGeneric = document.querySelector('.test-btn')!;
-console.log(btnGeneric);
+const taskForm = document.querySelector<HTMLFormElement>('.form');
+const formInput = document.querySelector<HTMLInputElement>('.form-input');
+const taskListElement = document.querySelector<HTMLUListElement>('.list');
 
-// Specific Element
-const btnEl = document.querySelector<HTMLButtonElement>('.test-btn')!;
+// task type
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
 
-btnEl.disabled = true;
-
-// Checking for Btn
-// Optional Chaining
-const btnElCheck = document.querySelector<HTMLButtonElement>('.test-btn');
-
-btnElCheck?.addEventListener('click', () => {
-  console.log('Testing button ');
-});
-
-// btnElCheck?.disabled = false;  // Results in TypeScript Error
-
-if (btnElCheck) {
-  btnElCheck.disabled = false;
-}
-
-// Type Assertion option
-const btnAssertion = document.querySelector('.test-btn')! as HTMLButtonElement;
-
-btnAssertion.disabled = true;
+const tasks: Task[] = [];
