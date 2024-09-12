32720a436290d3500529600af67dc102e91ac1f0
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-12 13:16:55 +0530

    component return types

---
+++ 03-React-TypeScript-Essentials/src/01-return/index.tsx
@@ -0,0 +1,11 @@
+const Component = (): JSX.Element | null | string => {
+  return null;
+  return 'Hello TypeScript';
+  return (
+    <div>
+      <h2>Return type</h2>
+    </div>
+  );
+};
+
+export default Component;
--- 03-React-TypeScript-Essentials/src/App.tsx
+++ 03-React-TypeScript-Essentials/src/App.tsx
@@ -1,7 +1,10 @@
+import Component from './01-return';
+
 function App() {
   return (
     <main>
       <h2>React & TypeScript</h2>
+      <Component />
     </main>
   );
 }
