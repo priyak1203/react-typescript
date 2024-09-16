c357e7f7a5088a81bc529c0d466e7a7734e8d356
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-16 16:12:00 +0530

    profile card types solution

--- 03-React-TypeScript-Essentials/src/App.tsx
+++ 03-React-TypeScript-Essentials/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
       <h2>React & TypeScript</h2>
       <Component type="basic" name="anna" />
       <Component type="advanced" name="Susan" email="susan@gmail.com" />
-      <Component type="advanced" name="David" />
     </main>
   );
 }
