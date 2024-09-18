142b3b1ae9e5dfd9d5ed11182aa2c233b3ee0c26
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-18 14:43:30 +0530

    basic setup without reducer

---
+++ 03-React-TypeScript-Essentials/src/07-reducers/index.tsx
@@ -0,0 +1,35 @@
+const Component = () => {
+  return (
+    <div>
+      <h2>Count: 0</h2>
+      <h2>Status: Active</h2>
+      <div className="btn-container">
+        <button onClick={() => console.log('increment')} className="btn">
+          increment
+        </button>
+        <button onClick={() => console.log('decrement')} className="btn">
+          decrement
+        </button>
+        <button onClick={() => console.log('reset')} className="btn">
+          reset
+        </button>
+      </div>
+      <div className="btn-container">
+        <button
+          className="btn"
+          onClick={() => console.log('set status to active')}
+        >
+          set status to active
+        </button>
+        <button
+          className="btn"
+          onClick={() => console.log('set status to inactive')}
+        >
+          set status to inactive
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Component;
