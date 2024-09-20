79c68692ae517a204299c351d8e61029264ae20e
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-20 13:09:44 +0530

    fetch data with axios and react query

--- 03-React-TypeScript-Essentials/src/08-fetch-data/index.tsx
+++ 03-React-TypeScript-Essentials/src/08-fetch-data/index.tsx
@@ -1,52 +1,23 @@
-import { useEffect, useState } from 'react';
-import { tourSchema, type Tour } from './types';
-
-const url = 'https://www.course-api.com/react-tours-project';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTours } from './types';
 
 const Component = () => {
-  const [tours, setTours] = useState<Tour[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<string | null>(null);
+  const {
+    isPending,
+    isError,
+    error,
+    data: tours,
+  } = useQuery({
+    queryKey: ['tours'],
+    queryFn: fetchTours,
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-          throw new Error(`Failed to fetch tours`);
-        }
-
-        const rawData: Tour[] = await response.json();
-
-        const result = tourSchema.array().safeParse(rawData);
-
-        if (!result.success) {
-          console.log(result.error.message);
-          throw new Error(`Failed to parse tours`);
-        }
-        setTours(result.data);
-        setIsError(null);
-      } catch (error) {
-        console.log(error);
-        const message =
-          error instanceof Error ? error.message : `there was an error...`;
-        setIsError(message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (isLoading) {
+  if (isPending) {
     return <h3>Loading...</h3>;
   }
 
   if (isError) {
-    return <h3>Error: {isError}</h3>;
+    return <h3>Error: {error.message}</h3>;
   }
 
   return (
--- 03-React-TypeScript-Essentials/src/08-fetch-data/types.ts
+++ 03-React-TypeScript-Essentials/src/08-fetch-data/types.ts
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import { z } from 'zod';
+
+const url = 'https://www.course-api.com/react-tours-project';
 
 export const tourSchema = z.object({
   id: z.string(),
@@ -10,3 +13,12 @@ export const tourSchema = z.object({
 });
 
 export type Tour = z.infer<typeof tourSchema>;
+
+export const fetchTours = async (): Promise<Tour[]> => {
+  const response = await axios.get<Tour[]>(url);
+  const result = tourSchema.array().safeParse(response.data);
+  if (!result.success) {
+    throw new Error(`Parsing Failed!`);
+  }
+  return result.data;
+};
