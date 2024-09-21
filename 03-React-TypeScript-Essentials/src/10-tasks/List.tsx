5bcab76e67f844d3b75d6af101e1d4e9d3462061
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-21 13:16:04 +0530

    rendering tasks and toggle task function implementation

--- 03-React-TypeScript-Essentials/src/10-tasks/List.tsx
+++ 03-React-TypeScript-Essentials/src/10-tasks/List.tsx
@@ -1,5 +1,27 @@
-const List = () => {
-  return <div>List</div>;
+import { type Task } from './types';
+
+type ListProps = {
+  tasks: Task[];
+  toggleTask: ({ id }: { id: string }) => void;
+};
+
+const List = ({ tasks, toggleTask }: ListProps) => {
+  return (
+    <ul className="list">
+      {tasks.map((task) => {
+        return (
+          <li key={task.id}>
+            <p className="task-text">{task.description}</p>
+            <input
+              type="checkbox"
+              checked={task.isCompleted}
+              onChange={() => toggleTask({ id: task.id })}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default List;
--- 03-React-TypeScript-Essentials/src/10-tasks/index.tsx
+++ 03-React-TypeScript-Essentials/src/10-tasks/index.tsx
@@ -10,10 +10,21 @@ const Component = () => {
     setTasks([...tasks, task]);
   };
 
+  const toggleTask = ({ id }: { id: string }) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, isCompleted: !task.isCompleted };
+        }
+        return task;
+      })
+    );
+  };
+
   return (
     <section>
       <Form addTask={addTask} />
-      <List />
+      <List tasks={tasks} toggleTask={toggleTask} />
     </section>
   );
 };
